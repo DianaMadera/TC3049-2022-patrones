a5a4d3066472dffa33c49bc4bcd338ffeeceab94
DianaMadera <31284145+DianaMadera@users.noreply.github.com>
2022-11-21 16:58:10 -0600

    singletone

---
+++ singletone/Program.cs
@@ -0,0 +1,44 @@
+﻿//crear espacio de trabajo
+namespace Patrones
+{
+    class MainApp
+    {
+        static void Main(){
+            //se crea la instancia
+            Singleton? transaction1= Singleton.Instance();
+            transaction1.PonerDatos(1);
+            Console.WriteLine(transaction1.ToString());
+            Singleton? transaction2= Singleton.Instance();
+        }
+    }
+    
+    internal class Singleton
+    {
+
+        private int no_transaction;
+
+        internal static Singleton? _instance;
+        //verificar que la instancia solo se ejecuto o creo una vez
+        internal static Singleton? Instance()
+        {
+            if(_instance == null){
+                _instance= new Patrones.Singleton();
+                Console.WriteLine("Nueva transacción en proceso");
+            }
+            else
+            {
+                Console.WriteLine("No se puede realizar la transacción porque otra se esta realizando");
+            }
+            return _instance;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("El cajero esta procesando la transacción {0} en este momento", no_transaction);
+        }
+        public void PonerDatos(int number)
+        {
+            no_transaction = number;
+        }
+    }
+}
